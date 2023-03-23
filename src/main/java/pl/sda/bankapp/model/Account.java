9c6fad23ac8431dd732998f8bc2e56af273889a4
szjozi <szjozefczuk.sda@gmail.com>
2023-03-23 21:26:33 +0100

    Account class added

---
+++ src/main/java/pl/sda/bankapp/model/Account.java
@@ -0,0 +1,112 @@
+package pl.sda.bankapp.model;
+
+import pl.sda.bankapp.enums.AccountType;
+import pl.sda.bankapp.enums.Currency;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Account {
+
+    private long id;
+    private long customerId;
+    private String accountNumber;
+    private Currency currency;
+    private AccountType accountType;
+    private BigDecimal currentAmount;
+
+    public Account() {
+        this.currentAmount = BigDecimal.ZERO;
+    }
+
+    public Account(long id, long customerId, String accountNumber,
+                   Currency currency, BigDecimal currentAmount,
+                   AccountType accountType) {
+
+        this.id = id;
+        this.customerId = customerId;
+        this.accountNumber = accountNumber;
+        this.currency = currency;
+        this.currentAmount = currentAmount;
+        this.accountType = accountType;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(long customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public BigDecimal getCurrentAmount() {
+        return currentAmount;
+    }
+
+    public void setCurrentAmount(BigDecimal currentAmount) {
+        this.currentAmount = currentAmount;
+    }
+
+    public AccountType getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(AccountType accountType) {
+        this.accountType = accountType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return id == account.id &&
+                customerId == account.customerId &&
+                Objects.equals(accountNumber, account.accountNumber) &&
+                currency == account.currency &&
+                Objects.equals(currentAmount, account.currentAmount) &&
+                accountType == account.accountType;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, customerId, accountNumber, currency,
+                currentAmount, accountType);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", customerId=" + customerId +
+                ", accountNumber='" + accountNumber + '\'' +
+                ", currency=" + currency +
+                ", currentAmount=" + currentAmount +
+                ", accountType=" + accountType +
+                '}';
+    }
+}
