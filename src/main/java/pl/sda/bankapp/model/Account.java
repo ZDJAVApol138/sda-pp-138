ac71e66707342625cfb3a0cb8276dd36e3fc2bae
szjozi <szjozefczuk.sda@gmail.com>
2023-04-18 18:19:17 +0200

    Task 7, 8, 9.

--- src/main/java/pl/sda/bankapp/model/Account.java
+++ src/main/java/pl/sda/bankapp/model/Account.java
@@ -1,12 +1,15 @@
 package pl.sda.bankapp.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import pl.sda.bankapp.enums.AccountType;
 import pl.sda.bankapp.enums.Currency;
 import pl.sda.bankapp.utils.AccountNumberGenerator;
 
 import java.math.BigDecimal;
-import java.util.Objects;
 
+@Data
+@NoArgsConstructor
 public abstract class Account {
 
     private long id;
@@ -15,9 +18,6 @@ public abstract class Account {
     private AccountType accountType;
     private BigDecimal currentAmount = BigDecimal.ZERO;
     private final String accountNumber = AccountNumberGenerator.generate();
-
-    public Account() {
-    }
 
     public Account(long id, long customerId, Currency currency, AccountType accountType) {
         this.id = id;
@@ -36,80 +36,5 @@ public abstract class Account {
         if (currentAmount.compareTo(withdrawAmount) >= 0) {
             currentAmount = currentAmount.subtract(withdrawAmount);
         }
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public long getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(long customerId) {
-        this.customerId = customerId;
-    }
-
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public Currency getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(Currency currency) {
-        this.currency = currency;
-    }
-
-    public BigDecimal getCurrentAmount() {
-        return currentAmount;
-    }
-
-    public void setCurrentAmount(BigDecimal currentAmount) {
-        this.currentAmount = currentAmount;
-    }
-
-    public AccountType getAccountType() {
-        return accountType;
-    }
-
-    public void setAccountType(AccountType accountType) {
-        this.accountType = accountType;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Account account = (Account) o;
-        return id == account.id &&
-                customerId == account.customerId &&
-                Objects.equals(accountNumber, account.accountNumber) &&
-                currency == account.currency &&
-                Objects.equals(currentAmount, account.currentAmount) &&
-                accountType == account.accountType;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, customerId, accountNumber, currency,
-                currentAmount, accountType);
-    }
-
-    @Override
-    public String toString() {
-        return "Account{" +
-                "id=" + id +
-                ", customerId=" + customerId +
-                ", accountNumber='" + accountNumber + '\'' +
-                ", currency=" + currency +
-                ", currentAmount=" + currentAmount +
-                ", accountType=" + accountType +
-                '}';
     }
 }
--- src/main/java/pl/sda/bankapp/model/Customer.java
+++ src/main/java/pl/sda/bankapp/model/Customer.java
@@ -1,15 +1,19 @@
 package pl.sda.bankapp.model;
 
+import lombok.*;
+
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Customer {
 
     private long id;
-    private int age;
     private String name;
     private String surname;
     private String phone;
@@ -18,22 +22,7 @@ public class Customer {
     private Address address;
     private LocalDate dateOfBirth;
     private final List<Account> accounts = new ArrayList<>();
-
-    public Customer() {
-    }
-
-    public Customer(long id, String name, String surname, String phone,
-                    String email, String pesel, Address address, LocalDate dateOfBirth) {
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-        this.phone = phone;
-        this.email = email;
-        this.pesel = pesel;
-        this.address = address;
-        this.dateOfBirth = dateOfBirth;
-        this.age = Period.between(dateOfBirth, LocalDate.now()).getYears();
-    }
+    private final int age = Period.between(dateOfBirth, LocalDate.now()).getYears();
 
 //    private int calculateAge() {
 //        LocalDate now = LocalDate.now();
@@ -63,114 +52,5 @@ public class Customer {
 //        }
 
         accounts.forEach(System.out::println);
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPesel() {
-        return pesel;
-    }
-
-    public void setPesel(String pesel) {
-        this.pesel = pesel;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Customer customer = (Customer) o;
-        return id == customer.id &&
-                age == customer.age &&
-                Objects.equals(name, customer.name) &&
-                Objects.equals(surname, customer.surname) &&
-                Objects.equals(phone, customer.phone) &&
-                Objects.equals(email, customer.email) &&
-                Objects.equals(pesel, customer.pesel) &&
-                Objects.equals(address, customer.address) &&
-                Objects.equals(dateOfBirth, customer.dateOfBirth);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, age, name, surname, phone, email,
-                pesel, address, dateOfBirth);
-    }
-
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "id=" + id +
-                ", age=" + age +
-                ", name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", phone='" + phone + '\'' +
-                ", email='" + email + '\'' +
-                ", pesel='" + pesel + '\'' +
-                ", address=" + address +
-                ", dateOfBirth=" + dateOfBirth +
-                '}';
     }
 }
